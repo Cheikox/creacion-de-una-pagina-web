e911b25de69aabd9bfd9612620804fc53bbbb6f1
Cheikox <103221876+Cheikox@users.noreply.github.com>
2022-04-29 15:35:18 -0400

    Add files via upload

---
+++ Proyecto/proyec.js
@@ -0,0 +1,28 @@
+function validarEdad(){
+    let edad = document.getElementById("edad").value
+    let c = [1, 2, 3]
+    let c = [1, 2, 3]
+    let c = [1, 2, 3]
+    let arreglo = [c, d, e]
+
+    //let arreglo
+    
+    let edad = document.getElementById("edad").
+    value
+    //validadcion de edad
+
+    if (edad > 17){
+        document.getElementById("respuesta").
+        innerHTML = "es mayor de edad"}
+    else{
+        document.getElementById ("respuesta").
+        innerHTML = "Es menos de edad"
+    }
+    // Validacion de genero
+    let genero = document.getElementById("genero")
+    console.log("Arreglo.length"+arreglo-length)
+    for(let x =0; x < arreglo.length; x++){
+        console.log(arreglo[x])
+    }
+
+}
